e58e41668f28dde897a5652994636ffde476d372
Paul Ambrose <pambrose@mac.com>
2017-02-05 15:39:46 -0800

    Fix http arg

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -5,5 +5,6 @@ workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python2 ~pi/git/object-tracking/single_object_tracker.py --bgr "174, 56, 5" --width 400 --flipy --usb --http "raspi11.local:8080" &> ~pi/git/FRC-2017/logs/object-tracker.out &
 
+
 # 174, 56, 5 is blue
 # 46, 43, 144 is red box
