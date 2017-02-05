a3077c329584d68f4848d16587ea0a6e315686c3
Paul Ambrose <pambrose@mac.com>
2017-02-05 14:23:40 -0800

    Cleanup lidar  scripts

--- bin/lidar-left-publisher.sh
+++ bin/lidar-left-publisher.sh
@@ -1,6 +1,5 @@
-#! /bin/bash
+#!/bin/bash
 
-source ~pi/.profile
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python3 ~pi/git/FRC-2017/lidar_publisher.py --mqtt mqtt-turtle.local --device lidar_l --serial ttyACM0 &> ~pi/git/FRC-2017/logs/lidar-left-publisher.out &
 
