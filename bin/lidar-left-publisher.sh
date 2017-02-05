ac6bac7e147f365e7b7d4fe26dd5d76f1b68871b
Paul Ambrose <pambrose@mac.com>
2017-02-05 14:27:50 -0800

    Make lidar scripts executable

--- bin/lidar-left-publisher.sh
+++ bin/lidar-left-publisher.sh
@@ -3,3 +3,4 @@
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python3 ~pi/git/FRC-2017/lidar_publisher.py --mqtt mqtt-turtle.local --device lidar_l --serial ttyACM0 &> ~pi/git/FRC-2017/logs/lidar-left-publisher.out &
 
+
