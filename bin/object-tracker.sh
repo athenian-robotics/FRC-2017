1dae1c4aa0ed8e253ae7e901af9680cc49610aea
Paul Ambrose <pambrose@mac.com>
2017-02-06 10:40:42 -0800

    Add http delay arg

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -3,7 +3,7 @@
 source ~pi/.profile
 workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/single_object_tracker.py --bgr "174, 56, 5" --width 400 --flipy --usb --http "raspi11.local:8080" &> ~pi/git/FRC-2017/logs/object-tracker.out &
+python2 ~pi/git/object-tracking/single_object_tracker.py --bgr "174, 56, 5" --width 400 --delay 0.25 --flipy --usb --http "raspi11.local:8080" &> ~pi/git/FRC-2017/logs/object-tracker.out &
 
 
 # 174, 56, 5 is blue
