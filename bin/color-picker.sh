c1a5dd64223f92ac13463ee759c6bada28e3bf13
Paul Ambrose <pambrose@mac.com>
2017-02-06 17:42:35 -0800

    Add args for html_path on raspi

--- bin/color-picker.sh
+++ bin/color-picker.sh
@@ -3,4 +3,4 @@
 source ~pi/.profile
 workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-~pi/git/object-tracking/color_picker.py -w 600
+~pi/git/object-tracking/color_picker.py --width 600 --flipy --path /home/pi/git/object-tracking/html
