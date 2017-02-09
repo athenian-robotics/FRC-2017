94567f40c032258782e85f1585878529b4c3d623
Paul Ambrose <pambrose@mac.com>
2017-02-08 19:47:51 -0800

    Add echos

--- bin/pull-from-github.sh
+++ bin/pull-from-github.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Push common-robotics repo
+echo "Updating common-robotics"
 cd ~/git/common-robotics
 git pull origin
 
 # Push FRC-2017 repo
+echo "Updating FRC-2017"
 cd ~/git/FRC-2017
 git pull origin
 
 # Push object-tracking repo
+echo "Updating object-tracking"
 cd ~/git/object-tracking
 git pull origin
 
