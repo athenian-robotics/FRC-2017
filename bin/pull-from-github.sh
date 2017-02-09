3b1eb868f52309bec7e137adef48493008c4a697
Paul Ambrose <pambrose@mac.com>
2017-02-08 16:59:54 -0800

    Add pull-from-github.sh

---
+++ bin/pull-from-github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Push common-robotics repo
+cd ~/git/common-robotics
+git pull origin
+
+# Push FRC-2017 repo
+cd ~/git/FRC-2017
+git pull origin
+
+# Push object-tracking repo
+cd ~/git/object-tracking
+git pull origin
+
+
