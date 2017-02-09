161e799aa4446bd226c12ba6cb13820ebf9c479b
Paul Ambrose <pambrose@mac.com>
2017-02-08 16:26:43 -0800

    Add uber push script

---
+++ bin/push-to-robot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Push common-robotics repo
+cd ~/git/common-robotics
+git push camera-gear master
+git push lidar-gear-left master
+git push lidar-gear-rightg master
+
+# Push FCR-2017 repo
+cd ~/git/FRC-2017
+git push camera-gear master
+git push lidar-gear-left master
+git push lidar-gear-rightg master
+
+# Push object-tracking repo
+cd ~/git/object-tracking
+git push camera-gear master
+
+# Reboot machines
+ssh pi@camera-gear sudo reboot now
+ssh pi@lidar-gear-left sudo reboot now
+ssh pi@lidar-gear-right sudo reboot now
