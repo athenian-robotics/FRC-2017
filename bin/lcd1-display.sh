59daab17827bfdf217649781f0cff9d983ae1ef5
Paul Ambrose <pambrose@mac.com>
2017-02-12 17:36:18 -0800

    Add boot timestamp

--- bin/lcd1-display.sh
+++ bin/lcd1-display.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+date > ~pi/git/FRC-2017/logs/lcd1-display.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
 python3 ~pi/git/FRC-2017/lidarDisplay.py --mqtt mqtt-turtle.local &> ~pi/git/FRC-2017/logs/lidarDisplay.out &
