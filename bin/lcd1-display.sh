2233d27bc97331df844dd9c5940084499f5f447d
Paul Ambrose <pambrose@mac.com>
2017-02-13 09:23:54 -0800

    Rename lcd1 reboot file

--- bin/lcd1-display.sh
+++ bin/lcd1-display.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-date > ~pi/git/FRC-2017/logs/lcd1-display.reboot
+date > ~pi/git/FRC-2017/logs/lidar_display.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
 python3 ~pi/git/FRC-2017/lidar_display.py --mqtt mqtt-turtle.local &> ~pi/git/FRC-2017/logs/lidar_display.out &
