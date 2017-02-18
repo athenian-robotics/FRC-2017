07b4f237f1889f5f1e50977b980940ada2e36989
Paul Ambrose <pambrose@mac.com>
2017-02-17 18:23:52 -0800

    Keep bad data out of moving average

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -5,6 +5,5 @@ date > ~pi/git/FRC-2017/logs/object-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python2 ~pi/git/object-tracking/single_object_filter.py --bgr "174, 56, 5" --width 400 --delay 0.25 --flipy --usb --http "camera-gear.local:80" &> ~pi/git/FRC-2017/logs/object-filter.out &
 
-
 # 174, 56, 5 is blue
 # 46, 43, 144 is red box
