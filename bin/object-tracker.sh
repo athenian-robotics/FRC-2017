9b37db64ac9fdbe19ebb19ac72e92e6bf6db4640
Paul Ambrose <pambrose@mac.com>
2017-02-18 15:26:50 -0800

    Add --camera_name to object-tracker.sh

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -3,7 +3,7 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/object-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/single_object_filter.py --bgr "59, 66, 197"  --width 400 --delay 0.25 --flipy --usb --http "camera-gear.local:5800" &> ~pi/git/FRC-2017/logs/object-filter.out &
+python2 ~pi/git/object-tracking/single_object_filter.py --bgr "59, 66, 197"  --width 400 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" &> ~pi/git/FRC-2017/logs/object-filter.out &
 
 # 174, 56, 5 is blue
 # 46, 43, 144 is red box
