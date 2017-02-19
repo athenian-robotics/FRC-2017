10488d7bc3059fedebabe151ff7ee50315e9bb79
Paul Ambrose <pambrose@mac.com>
2017-02-18 21:05:01 -0800

    * Change boot order * Fix log name

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source ~pi/.profile
 workon py2cv3
-date > ~pi/git/FRC-2017/logs/object-tracker.reboot
+date > ~pi/git/FRC-2017/logs/object-filter.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python2 ~pi/git/object-tracking/single_object_filter.py --bgr "174, 56, 5"  --width 400 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" &> ~pi/git/FRC-2017/logs/object-filter.out &
 
