1cdc9caefe06ed8e4db692a4ee14af5e71a774e6
Paul Ambrose <pambrose@mac.com>
2017-02-19 10:34:40 -0800

    Add sleep 5 to boot scripts

--- bin/color-picker.sh
+++ bin/color-picker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source ~pi/.profile
 workon py2cv3
+sleep 5
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 ~pi/git/object-tracking/color_picker.py --width 600 --flipy --path /home/pi/git/object-tracking/html
--- bin/gear-front-publisher.sh
+++ bin/gear-front-publisher.sh
@@ -2,6 +2,7 @@
 source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/gear-front-publisher.reboot
+sleep 5
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python2 ~pi/git/FRC-2017/location_publisher.py --grpc camera-gear.local --camera camera/gear --mqtt mqtt-turtle.local &> ~pi/git/FRC-2017/logs/gear-front-publisher.out &
 
