fe979d00060188e868751aa4d1bedf37fe153b93
Paul Ambrose <pambrose@mac.com>
2017-02-20 14:47:13 -0800

    Add clear_logs.sh

---
+++ bin/clear_logs.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm  ~pi/git/FRC-2017/logs/*.out ~pi/git/FRC-2017/logs/*.reboot
+
--- bin/dual-tape-tracker.sh
+++ bin/dual-tape-tracker.sh
@@ -3,5 +3,5 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dual-tape-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "172, 220, 14"  --mask_x 10  --draw_contour --draw_box --sleep 60 --width 500 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
+python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "172, 220, 14"  --mask_y 10  --draw_contour --draw_box --sleep 60 --width 500 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
 
