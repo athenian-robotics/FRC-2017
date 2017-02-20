909075465832aaea6be1691cbd5614ded4c3149c
Paul Ambrose <pambrose@mac.com>
2017-02-20 11:35:48 -0800

    Add box and contours

--- bin/dual-tape-tracker.sh
+++ bin/dual-tape-tracker.sh
@@ -3,5 +3,5 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dual-tape-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "214, 201, 172"  -- countours --box --sleep 90 --width 500 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
+python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "214, 201, 172"  --draw_contour --draw_box --sleep 90 --width 500 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
 
