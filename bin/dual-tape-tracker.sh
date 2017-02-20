7b01a99862e590fa58aa85ace73c2d0bf811c957
Paul Ambrose <pambrose@mac.com>
2017-02-19 17:36:05 -0800

    Add sleep to camera-gear

--- bin/dual-tape-tracker.sh
+++ bin/dual-tape-tracker.sh
@@ -2,7 +2,7 @@
 source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dual-tape-tracker.reboot
-sleep 5
+sleep 90
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "214, 201, 172"  --width 500 --delay 0.25 --flipy --usb --camera "Front Gear" --http "camera-gear.local:5800" &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
 
