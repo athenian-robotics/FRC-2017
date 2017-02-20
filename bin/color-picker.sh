662a81f19d4250a56e8e0305e982392926656821
Paul Ambrose <pambrose@mac.com>
2017-02-20 12:49:24 -0800

    Adjust sleep times

--- bin/color-picker.sh
+++ bin/color-picker.sh
@@ -3,4 +3,4 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dualtape-peg-color-picker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-~pi/git/object-tracking/color_picker.py --sleep 60 --usb --width 600 --flipy --camera "Front Gear" --http "camera-gear.local:5800" &> ~pi/git/FRC-2017/logs/color-picker.out &
+~pi/git/object-tracking/color_picker.py --sleep 60 --usb --width 600 --flipy --http "camera-gear.local:5800" &> ~pi/git/FRC-2017/logs/color-picker.out &
