86f68ea959112e778f66ff6a519453ae9d90e388
Paul Ambrose <pambrose@mac.com>
2017-02-20 13:03:15 -0800

    Switch to dual tape peg

--- bin/dual-tape-peg-object-tracker.sh
+++ bin/dual-tape-peg-object-tracker.sh
@@ -3,7 +3,7 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dualtape-peg-object-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/multi_object_tracker.py --dualbgr "172, 220, 14" --singlebgr "126, 113, 116" --draw_contour --draw_box --sleep 60 --width 500 --delay 0.25 --flipy --usb --http "camera-gear.local:8080" --vertical &> ~pi/git/FRC-2017/logs/dualtape-peg-object-tracker.out &
+python2 ~pi/git/object-tracking/multi_object_tracker.py --dualbgr "172, 220, 14" --singlebgr "126, 113, 116" --draw_contour --draw_box --sleep 60 --width 500 --delay 0.25 --flipy --usb --http "camera-gear.local:5800" --vertical &> ~pi/git/FRC-2017/logs/dualtape-peg-object-tracker.out &
 
 
 # 174, 56, 5 is blue
