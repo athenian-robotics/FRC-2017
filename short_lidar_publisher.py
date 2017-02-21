056f8f2e18e0ecde9addc5182beb261b9b61d641
Paul Ambrose <pambrose@mac.com>
2017-02-20 18:34:46 -0800

    Update lidar calcs

--- short_lidar_publisher.py
+++ short_lidar_publisher.py
@@ -15,7 +15,7 @@ from utils import sleep
 logger = logging.getLogger(__name__)
 
 last_val = 0
-moving_avg = MovingAverage(size=5)
+moving_avg = MovingAverage(size=3)
 bad_values = BadValuesQueue()
 
 SERIAL_READER = "serial_reader"
@@ -64,7 +64,7 @@ def fetch_data(mm_str, userdata):
         client.publish(topic, payload=str(mm).encode("utf-8"), qos=0)
     else:
         if abs(mm - avg) > TOLERANCE_THRESH:
-            client.publish(topic, payload=str(int(avg)).encode("utf-8"), qos=0)
+            client.publish(topic, payload=str(int(mm)).encode("utf-8"), qos=0)
 
 
 if __name__ == "__main__":
