d8dbe75cb5301b05a7e0d728cda091894c50108e
Paul Ambrose <pambrose@mac.com>
2017-02-22 11:15:55 -0800

    Update docs

--- boot/lidar-gear-startup.sh
+++ boot/lidar-gear-startup.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+# Publish lidar data
+su - pi -c ~pi/git/FRC-2017/bin/lidar-right-publisher.sh
+su - pi -c ~pi/git/FRC-2017/bin/lidar-left-publisher.sh
+
+# Publish heading data
+su - pi -c ~pi/git/FRC-2017/bin/heading-publisher.sh
