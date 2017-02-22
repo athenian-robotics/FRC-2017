71c70f66072992ef5f71805e606f592ed892fabc
Paul Ambrose <pambrose@mac.com>
2017-02-21 20:37:52 -0800

    Update html page

--- bin/dual-tape-tracker.sh
+++ bin/dual-tape-tracker.sh
@@ -4,5 +4,5 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dual-tape-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/dual_object_filter.py  --mask_x 15 --file /home/pi/git/FRC-2017/html/2-image.html --bgr "172, 220, 14"  --mask_y 0  --draw_contour --draw_box --sleep 0 --width 500 --delay 0.25 --flipy --usb --camera "Gear Dual Tape" --http "127.0.0.1:8080" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
+python2 ~pi/git/object-tracking/dual_object_filter.py  --mask_y 15 --file /home/pi/git/FRC-2017/html/2-image.html --bgr "172, 220, 14"  --mask_y 0  --draw_contour --draw_box --sleep 0 --width 500 --delay 0.25 --flipy --usb --camera "Gear Dual Tape" --http "127.0.0.1:8080" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
 
