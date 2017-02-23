cccb2a4a46c3900ca5052aad3881e40fbb82046d
Paul Ambrose <pambrose@mac.com>
2017-02-22 21:57:36 -0800

    Reorder reboot

--- bin/reboot-robot.sh
+++ bin/reboot-robot.sh
@@ -3,7 +3,7 @@
 # Reboot Robot Raspis
 echo "Rebooting Robot Raspis"
 ssh -o StrictHostKeyChecking=no pi@mqtt-turtle.local sudo reboot now
-ssh -o StrictHostKeyChecking=no pi@camera-gear.local sudo reboot now
 ssh -o StrictHostKeyChecking=no pi@camera-rope.local sudo reboot now
+ssh -o StrictHostKeyChecking=no pi@camera-gear.local sudo reboot now
 ssh -o StrictHostKeyChecking=no pi@lidar-gear.local sudo reboot now
 ssh -o StrictHostKeyChecking=no pi@lcd1.local sudo reboot now
--- bin/shutdown-robot.sh
+++ bin/shutdown-robot.sh
@@ -3,7 +3,7 @@
 # Reboot Robot Raspis
 echo "Shutting Down Robot Raspis"
 ssh -o StrictHostKeyChecking=no pi@mqtt-turtle.local sudo shutdown now
-ssh -o StrictHostKeyChecking=no pi@camera-gear.local sudo shutdown now
 ssh -o StrictHostKeyChecking=no pi@camera-rope.local sudo shutdown now
+ssh -o StrictHostKeyChecking=no pi@camera-gear.local sudo shutdown now
 ssh -o StrictHostKeyChecking=no pi@lidar-gear.local sudo shutdown now
 ssh -o StrictHostKeyChecking=no pi@lcd1.local sudo shutdown now
