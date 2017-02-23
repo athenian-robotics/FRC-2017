f1102f8bd33d81c1bd3e3ceb0287d64b5901d114
Paul Ambrose <pambrose@mac.com>
2017-02-23 10:47:34 -0800

    Two camera experiment

--- bin/2nd-camera.sh
+++ bin/2nd-camera.sh
@@ -4,5 +4,5 @@ source ~pi/.profile
 workon py2cv3
 date > ~pi/git/FRC-2017/logs/dual-tape-tracker.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "172, 220, 14"  --draw_contour --draw_box --width 500 --delay 0.25 --flipy --usb_port 1 --camera "Gear Camera" --http "camera-gear.local:8082" --vertical &> ~pi/git/FRC-2017/logs/dual-tape-tracker.out &
+python2 ~pi/git/object-tracking/dual_object_filter.py --bgr "172, 220, 14"  --draw_contour --draw_box --width 500 --delay 0.25 --flipy --usb_port 1 --camera "Gear Camera" --http "camera-gear.local:8082" --vertical &> ~pi/git/FRC-2017/logs/2nd-camera.out &
 
