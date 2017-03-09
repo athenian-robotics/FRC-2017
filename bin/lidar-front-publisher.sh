de585418a35fc84c054271b7392a86befb4ddec1
Paul Ambrose <pambrose@mac.com>
2017-03-08 16:53:02 -0800

    Update long lidar publisher

--- bin/lidar-front-publisher.sh
+++ bin/lidar-front-publisher.sh
@@ -2,5 +2,5 @@
 
 date > ~pi/git/FRC-2017/logs/lidar-front-publisher.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
-#python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt mqtt-turtle.local --avg_size 10 --device front --device_id 00FEBA8B --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-front-publisher.out &
-python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt pleiku.local --avg_size 10 --device front --did 00FEBA8B --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-front-publisher.out &
+#python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt mqtt-turtle.local --avg_size 15 --device front --did 00FEBA8B --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-front-publisher.out &
+python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt pleiku.local --avg_size 15 --device front --did 00FEBA8B --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-front-publisher.out &
--- bin/lidar-rear-publisher.sh
+++ bin/lidar-rear-publisher.sh
@@ -2,5 +2,5 @@
 
 date > ~pi/git/FRC-2017/logs/lidar-rear-publisher.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
-#python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt mqtt-turtle.local --avg_size 10 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
-python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt pleiku.local --avg_size 10 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
+#python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt mqtt-turtle.local --avg_size 15 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
+python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt pleiku.local --avg_size 15 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
