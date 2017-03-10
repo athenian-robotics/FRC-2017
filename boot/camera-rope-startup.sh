445a57e6642d3e1688c7d49c7c36b7c755c6882e
Paul Ambrose <pambrose@mac.com>
2017-03-10 11:25:33 -0800

    Fix sensors

--- boot/camera-rope-startup.sh
+++ boot/camera-rope-startup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# Publish heading data
+su - pi -c ~pi/git/FRC-2017/bin/heading-publisher.sh
+
 su - pi -c ~pi/git/FRC-2017/bin/rope-tracker.sh
 #su - pi -c ~pi/git/FRC-2017/bin/rope-publisher.sh
 
-# Publish heading data
-su - pi -c ~pi/git/FRC-2017/bin/heading-publisher.sh
