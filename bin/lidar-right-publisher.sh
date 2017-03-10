188d5459443bd38317590c87d24ed902f57c33e7
Paul Ambrose <pambrose@mac.com>
2017-03-10 12:55:42 -0800

    Fix oor data

--- bin/lidar-right-publisher.sh
+++ bin/lidar-right-publisher.sh
@@ -2,4 +2,4 @@
 
 date > ~pi/git/FRC-2017/logs/lidar-right-publisher.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
-python3 ~pi/git/FRC-2017/short_lidar_publisher.py --mqtt mqtt-turtle.local --device right --did 95538333535351019130 --baud 115200  --oor_size 10 --oor_time 1000 --oor_upper 2000 &> ~pi/git/FRC-2017/logs/lidar-right-publisher.out &
+python3 ~pi/git/FRC-2017/short_lidar_publisher.py --mqtt mqtt-turtle.local --device right --did 95538333535351019130 --baud 115200  --oor_size 5 --oor_time 1000 --oor_upper 2000 &> ~pi/git/FRC-2017/logs/lidar-right-publisher.out &
