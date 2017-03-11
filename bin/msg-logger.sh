687d0cff17cb10d3621d10bcb4bfc8c2590fb922
Paul Ambrose <pambrose@mac.com>
2017-03-11 12:49:39 -0800

    Fix msg-writer

--- bin/msg-logger.sh
+++ bin/msg-logger.sh
@@ -2,5 +2,5 @@
 
 date > ~pi/git/FRC-2017/logs/msg_logger.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
-python ~pi/git/FRC-2017/heading_publisher.py --mqtt mqtt-turtle.local --dir ~pi/git/FRC-2017/logs &> ~pi/git/FRC-2017/logs/msg_logger.out &
+python ~pi/git/FRC-2017/msg_logger.py --mqtt mqtt-turtle.local --dir ~pi/git/FRC-2017/logs &> ~pi/git/FRC-2017/logs/msg_logger.out &
 
