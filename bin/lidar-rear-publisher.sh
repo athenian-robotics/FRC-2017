7ec646699e144398d89a89f06da840a6be8ccab0
Paul Ambrose <pambrose@mac.com>
2017-03-27 21:18:20 -0700

    Testing

--- bin/lidar-rear-publisher.sh
+++ bin/lidar-rear-publisher.sh
@@ -3,6 +3,7 @@
 date > ~pi/git/FRC-2017/logs/lidar-rear-publisher.reboot
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
 
-# PRA
-#python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt mqtt-turtle.local --avg_size 20 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
-python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt pleiku.local --avg_size 20 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
+python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt mqtt-turtle.local --avg_size 20 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
+
+# Testing
+# python3 ~pi/git/FRC-2017/long_lidar_publisher.py --mqtt pleiku.local --avg_size 20 --device rear --did 00FEBA73 --baud 115200 &> ~pi/git/FRC-2017/logs/lidar-rear-publisher.out &
--- boot/lcd1-startup.sh
+++ boot/lcd1-startup.sh
@@ -2,9 +2,9 @@
 
 su - pi -c ~pi/git/FRC-2017/bin/lcd1-display.sh
 
-# PRA
-su - pi -c ~pi/git/FRC-2017/bin/heading-publisher.sh
-su - pi -c ~pi/git/FRC-2017/bin/lidar-front-publisher.sh
-su - pi -c ~pi/git/FRC-2017/bin/lidar-rear-publisher.sh
+# Testing
+#su - pi -c ~pi/git/FRC-2017/bin/heading-publisher.sh
+#su - pi -c ~pi/git/FRC-2017/bin/lidar-front-publisher.sh
+#su - pi -c ~pi/git/FRC-2017/bin/lidar-rear-publisher.sh
 
 
