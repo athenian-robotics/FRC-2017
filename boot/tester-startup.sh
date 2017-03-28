e63a1399040f1d48d1897882b7cb5284d89ec648
Paul Ambrose <pambrose@mac.com>
2017-03-27 22:17:59 -0700

    Add scripts for tester raspi

---
+++ boot/tester-startup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+su - pi -c ~pi/git/FRC-2017/bin/tester-lcd-display.sh
+su - pi -c ~pi/git/FRC-2017/bin/tester-heading-publisher.sh
+su - pi -c ~pi/git/FRC-2017/bin/tester-lidar-front-publisher.sh
+su - pi -c ~pi/git/FRC-2017/bin/tester-lidar-rear-publisher.sh
+#su - pi -c ~pi/git/FRC-2017/bin/tester-lidar-left-publisher.sh
+
+
