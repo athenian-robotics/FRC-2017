3c869ac7950d0f431d40c84d0224b299aaf60cb0
Paul Ambrose <pambrose@mac.com>
2017-04-04 23:20:04 -0700

    Add go http-prozy to zero-lidar

--- bin/zero-lidar.sh
+++ bin/zero-lidar.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 date > ~pi/git/FRC-2017/logs/zero-lidar.reboot
+export PYTHONWARNINGS="ignore"
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
 export GOPATH=~pi/go
+export GOBIN=~pi/go/bin
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-export PYTHONWARNINGS="ignore"
+export PATH=/usr/local/go/bin:$PATH
 
 python ~pi/git/distance-tracking/distance_server.py --did 00FEBABC --baud 115200 &> ~pi/git/FRC-2017/logs/distance_server.out &
 
