85e7fbda4d5b33308212148a0c8f9cda43424b06
Paul Ambrose <pambrose@mac.com>
2017-06-05 13:06:36 -0700

    Update startup scripts

--- bin/zero-lidar-http.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-date > ~pi/git/FRC-2017/logs/zero-lidar-http.reboot
-export PYTHONWARNINGS="ignore"
-export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics
-
-python ~pi/git/distance-tracking/simple_distance_server.py --did 00FEBABC --baud 115200 &> ~pi/git/FRC-2017/logs/simple_distance_server.out &
-
